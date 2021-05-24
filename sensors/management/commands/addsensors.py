0a83282df0bbe494606a3e764f1042f5467ec3ff
manuo1 <manuo1@free.fr>
2021-05-24 19:58:47 +0200

    add custom django-admin commands : addsensors

---
+++ sensors/management/commands/addsensors.py
@@ -0,0 +1,32 @@
+
+import glob
+
+from pathlib import Path
+from django.core.management.base import BaseCommand, CommandError
+from sensors.models import TemperatureSensorManager
+
+temperature_sensor_manager = TemperatureSensorManager()
+
+class Command(BaseCommand):
+    help = """
+    Search and add sensors to the database (use for a first installation)
+    """
+
+    def add_arguments(self, addsensors):
+        pass
+
+    def handle(self, *args, **options):
+        """main controler."""
+        temperature_sensor_manager.add_sensors(self.all_temperature_sensors())
+
+    def all_temperature_sensors(self):
+        """ Search temperature sensors """
+        # Raspberry have folder with alls one wire devices at W1_DIRECTORY
+        W1_DIRECTORY = Path("/sys/bus/w1/devices")
+        # The data of each of the one-wire sensors is stored in files named by
+        # |     the sensor address and the DS18B20 sensors address start with
+        # |     the number 28
+        # glob module finds all the pathnames matching a specified pattern
+        sensor_folder_paths = glob.glob(str(W1_DIRECTORY)+"/28*")
+        sensor_folder_paths = ['/sys/bus/w1/devices/28-000005c7cc82', '/sys/bus/w1/devices/28-000005c83f61', '/sys/bus/w1/devices/28-000005c7d98e', '/sys/bus/w1/devices/28-000005c6f49f', '/sys/bus/w1/devices/28-000005c83686', '/sys/bus/w1/devices/28-000005c6d2aa', '/sys/bus/w1/devices/28-000005c7824f', '/sys/bus/w1/devices/28-000005c6d680']
+        return sensor_folder_paths
--- sensors/models.py
+++ sensors/models.py
@@ -1,10 +1,26 @@
-from django.db import models
+from django.db import models, IntegrityError
 from rooms.models import Room
+
+class TemperatureSensorManager(models.Manager):
+    def add_sensors(self, sensor_folder_paths):
+        for path in sensor_folder_paths:
+            temperature_sensor = TemperatureSensor(sensor_folder_path = path)
+            try:
+                temperature_sensor.save()
+            except IntegrityError:
+                pass
 
 class TemperatureSensor(models.Model):
 
-    name = models.CharField(max_length=100)
-    address = models.IntegerField(unique=True)
+    name = models.CharField(
+        default="unnamed",
+        max_length=100
+        )
+    sensor_folder_path = models.CharField(
+        max_length=100,
+        blank=True,
+        unique=True,
+        )
     last_measured_temperature = models.DecimalField(
         default=99.9,
         max_digits=3,
