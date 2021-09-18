07b84becb1d87d36653a5c05713c4b90af2fc041
manuo1 <manuo1@free.fr>
2021-09-18 16:14:28 +0200

    ad celery task for power remaining monitoring

--- housebrain_config/celery.py
+++ housebrain_config/celery.py
@@ -44,6 +44,10 @@ app.conf.beat_schedule = {
         'task': 'teleinformation.tasks.read_and_save_teleinformation',
         'schedule': crontab(minute='0'),
     },
+    'Check remaining power every 5 seconds': {
+        'task': 'teleinformation.tasks.check_remaining_power',
+        'schedule': 5,
+    },
 
 }
 
--- teleinformation/tasks.py
+++ teleinformation/tasks.py
@@ -5,6 +5,12 @@ from django.core import management
 
 @shared_task
 def read_and_save_teleinformation():
-    """ run the manage.py command : temperatures_save """
+    """ run the manage.py command : teleinfo_history_save """
     # periodiode is configured in django admin
     management.call_command('teleinfo_history_save')
+
+@shared_task
+def check_remaining_power():
+    """ run the manage.py command : power_monitoring """
+    # periodiode is configured in django admin
+    management.call_command('power_monitoring')
