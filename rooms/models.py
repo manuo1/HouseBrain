f7f1a979326d73deb660dafab93df44f868a20a1
manuo1 <manuo1@free.fr>
2021-09-27 20:52:47 +0200

    Change heaters model

--- rooms/models.py
+++ rooms/models.py
@@ -4,14 +4,24 @@ class Room(models.Model):
 
     class HeatingModeChoices(models.IntegerChoices):
         AUTO = 1, 'Auto'
-        MANUAL = 2, 'Manuel'
+        MANUAL = 2, 'Manual'
 
     name = models.CharField(max_length=100)
-    heating_priority = models.IntegerField(unique=True)
+    heating_priority = models.IntegerField(default=1)
     heating_mode = models.IntegerField(
         default=HeatingModeChoices.AUTO,
         choices=HeatingModeChoices.choices
         )
+    setpoint_temperature = models.IntegerField(default=5)
 
     def __str__(self):
-        return self.name
+        ret = (
+            self.name +
+            " | priority " +
+            str(self.heating_priority) +
+            " | mode : " +
+            str(self.heating_mode) +
+            " | setpoint_temperature : " +
+            str(self.setpoint_temperature) + "°C"
+        )
+        return ret
