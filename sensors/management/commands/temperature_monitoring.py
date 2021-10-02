18c9c870dc7b20a4b903ebc4c004aa9f7fb47061
manuo1 <manuo1@free.fr>
2021-10-02 23:20:42 +0200

    stop temperature reading if sensor is malfunctionning

--- sensors/management/commands/temperature_monitoring.py
+++ sensors/management/commands/temperature_monitoring.py
@@ -40,29 +40,32 @@ class Command(BaseCommand):
             temperature = DEBUG_TEMPERATURE
             self.stdout.write("reading temp in ---- UNPLUGGED_MODE ----")
         else :
-            try:
-                with open (
-                    sensor.sensor_folder_path + TEMPERATURE_FILE
-                ) as file:
-                    lines = file.readlines()
-                    if len(lines) >= 2:
-                        line_1_split = lines[0].split()
-                        crc="NO"
-                        if line_1_split:
-                            crc=line_1_split[-1]
-                        if crc == "YES":
-                            temperature = lines[1].split()[-1][2:]
-                            try:
-                                temperature = int(temperature)
-                            except ValueError:
+            if sensor.is_malfunctioning:
+                temperature = ERROR_TEMPERATURE
+            else:
+                try:
+                    with open (
+                        sensor.sensor_folder_path + TEMPERATURE_FILE
+                    ) as file:
+                        lines = file.readlines()
+                        if len(lines) >= 2:
+                            line_1_split = lines[0].split()
+                            crc="NO"
+                            if line_1_split:
+                                crc=line_1_split[-1]
+                            if crc == "YES":
+                                temperature = lines[1].split()[-1][2:]
+                                try:
+                                    temperature = int(temperature)
+                                except ValueError:
+                                    temperature = ERROR_TEMPERATURE
+                                    sensor_manager.add_an_error(sensor)
+                            else:
                                 temperature = ERROR_TEMPERATURE
                                 sensor_manager.add_an_error(sensor)
-                        else:
-                            temperature = ERROR_TEMPERATURE
-                            sensor_manager.add_an_error(sensor)
-            except FileNotFoundError:
-                temperature = ERROR_TEMPERATURE
-                sensor_manager.add_an_error(sensor)
+                except FileNotFoundError:
+                    temperature = ERROR_TEMPERATURE
+                    sensor_manager.add_an_error(sensor)
         if temperature != ERROR_TEMPERATURE:
             sensor_manager.reset_consecutive_errors(sensor)
         return temperature
