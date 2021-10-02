ef3ad55dabc9b4c415105c48bcdfa47d8953ca3c
manuo1 <manuo1@free.fr>
2021-10-03 01:17:59 +0200

    home page display change

--- housebrain/views.py
+++ housebrain/views.py
@@ -2,20 +2,32 @@ from django.utils import timezone
 from django.shortcuts import render
 
 from sensors.models import TemperatureSensorManager
+from heaters.models import HeaterManager
 
+heater_manager = HeaterManager()
 t_sensor_manager = TemperatureSensorManager()
 
 def homepage(request):
     temperatures = []
+    heaters_state = "?"
+
     for sensor in t_sensor_manager.all_sensors():
         if not sensor.last_measured_temperature:
             temperature = 85000
         if sensor.associated_room:
             name = sensor.associated_room.name
+            heaters = heater_manager.room_heaters(sensor.associated_room)
+            setpoint_temperature = sensor.associated_room.setpoint_temperature
+
+            if heaters[0].is_on:
+                heaters_state = "ON"
+            elif not heaters[0].is_on:
+                heaters_state = "OFF"
         else :
             name =f'{sensor.name} ({sensor.sensor_folder_path[-15:]})'
         temperatures.append(
-            (name , f'{(sensor.last_measured_temperature/1000):.2f}°C')
+            (f'( {heaters_state} ) {name} consigne : {(setpoint_temperature/1000):.2f}' ,
+            f'mesure : {(sensor.last_measured_temperature/1000):.2f}°C')
         )
     context = {
         'date_time': f'{timezone.now():%d/%m/%Y %H:%M}',
