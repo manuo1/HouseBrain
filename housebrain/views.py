928e5b123a405d59eaab51009d35d7f4526d4498
manuo1 <manuo1@free.fr>
2021-10-03 14:55:43 +0200

    change homepage display

--- housebrain/views.py
+++ housebrain/views.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 from django.shortcuts import render
-
+from housebrain_config.settings.constants import (ERROR_TEMPERATURE)
 from sensors.models import TemperatureSensorManager
 from heaters.models import HeaterManager
 
@@ -8,29 +8,44 @@ heater_manager = HeaterManager()
 t_sensor_manager = TemperatureSensorManager()
 
 def homepage(request):
-    temperatures = []
-    heaters_state = "?"
+    data = []
+    heaters_state = "No Heater"
+    setpoint_temperature = "-°C"
+    measured_temperature = "-°C"
+
 
     for sensor in t_sensor_manager.all_sensors():
-        if not sensor.last_measured_temperature:
-            temperature = 85000
+
+        if sensor.last_measured_temperature:
+            measured_temperature = format_temperature(
+                sensor.last_measured_temperature
+                )
+        else:
+            measured_temperature = format_temperature(
+                ERROR_TEMPERATURE
+                )
+        # if sensor have an associated room get data
         if sensor.associated_room:
             name = sensor.associated_room.name
             heaters = heater_manager.room_heaters(sensor.associated_room)
-            setpoint_temperature = sensor.associated_room.setpoint_temperature
+            setpoint_temperature = format_temperature(
+                sensor.associated_room.setpoint_temperature
+                )
             if heaters:
                 if heaters[0].is_on:
                     heaters_state = "ON"
                 elif not heaters[0].is_on:
                     heaters_state = "OFF"
+        # if sensor don't have  an associated room return sensor name and path
         else :
             name =f'{sensor.name} ({sensor.sensor_folder_path[-15:]})'
-        temperatures.append(
-            (f'( {heaters_state} ) {name} consigne : {(setpoint_temperature/1000):.2f}' ,
-            f'mesure : {(sensor.last_measured_temperature/1000):.2f}°C')
-        )
+        room = [heaters_state, name, setpoint_temperature, measured_temperature]
+        data.append(room)
     context = {
         'date_time': f'{timezone.now():%d/%m/%Y %H:%M}',
-        'temperatures': temperatures,
+        'data': data,
     }
     return render(request, 'homepage.html', context)
+
+def format_temperature(temperature):
+    return f'{(temperature/1000):.2f}°C'
