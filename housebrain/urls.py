766cf2f197b6c8f20fbdd38014d1296363417461
manuo1 <manuo1@free.fr>
2021-10-25 00:10:54 +0200

    prepare url and views for heating calendar + small changes on display

--- housebrain/urls.py
+++ housebrain/urls.py
@@ -7,4 +7,6 @@ app_name = 'housebrain'
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('heating_periods/<int:heating_mode_id>', views.heating_periods, name='heating_periods'),
+    path('heating_calendar', views.heating_calendar, name='heating_calendar'),
+
 ]
