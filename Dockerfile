f1cdf67b2b0082b09c341e352fb201bc99853cfe
Emmanuel Oudot <emmanuel.oudot@ecoco2.com>
2024-10-12 18:20:05 +0200

    config changes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.13-slim
+FROM python:3.12-slim
 
 # Prévenir le buffering et l'écriture des fichiers .pyc
 ENV PYTHONUNBUFFERED=1
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+# Fonction pour vérifier la connexion à PostgreSQL
+postgres_ready() {
+python << END
+import sys
+import psycopg2
+from os import getenv
+
+try:
+    conn = psycopg2.connect(
+        dbname=getenv("POSTGRES_DB"),
+        user=getenv("POSTGRES_USER"),
+        password=getenv("POSTGRES_PASSWORD"),
+        host="postgres"
+    )
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+
+# Attendre que PostgreSQL soit prêt
+until postgres_ready; do
+  echo "Waiting for PostgreSQL..."
+  sleep 2
+done
+
 # Si le fichier .env n'existe pas créer depuis .env.exemple
 if [ ! -f ".env" ]; then
     echo ".env file not found. Copying .env.example to .env..."
