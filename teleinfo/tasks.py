d3abdb29fed21187d2fa00ec784c6462256cc978
Emmanuel Oudot <emmanuel.oudot@ecoco2.com>
2024-10-19 00:34:31 +0200

    some config change

--- teleinfo/tasks.py
+++ teleinfo/tasks.py
@@ -1,11 +1,12 @@
 import logging
 import time
+from celery import shared_task
 
 logger = logging.getLogger("django")
 
 
+@shared_task
 def test_task():
     while True:
         logger.info("hello i'm in a task in celery")
-        print("truc")
         time.sleep(10)  # Attente de 10 secondes ent
