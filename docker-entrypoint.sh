8346247cc19bbeeed7d4f771737599de6765589e
Emmanuel Oudot <emmanuel.oudot@ecoco2.com>
2024-10-19 00:34:31 +0200

    some config change

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,6 +17,18 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+# Créer un superutilisateur si nécessaire
+echo "Checking for superuser..."
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
+    print('Superuser created.')
+else:
+    print('Superuser already exists.')
+END
+
 # Lancer le serveur Django
 echo "Starting Django server..."
 exec python manage.py runserver 0.0.0.0:8000
